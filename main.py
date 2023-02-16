eb1021858828a4ec80828a9f175e49a471ce17b5
DeLornk <40305556+DeLornk@users.noreply.github.com>
2023-02-16 18:11:05 +0300

    Add clases for tasks and nodes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import copy
 from structures import *
 
 def get_brown(x_s, y_s, x_e, y_e, w, n=1):
@@ -52,18 +51,75 @@ def generate_tasks(list_node_ids: list[str]) -> list[tuple]:
     for i in list_node_ids:
         # количество задач на узел по экспоненциальному распределению
         count_tasks_node_i = int(np.random.exponential(scale=2.0, size=1))
+        classes_tasks = np.random.choice(4, count_tasks_node_i, p=[0.3, 0.4, 0.2, 0.1])
         for j in range(count_tasks_node_i):
-            # распределение вычислительной сложности задачи?
-            calc_size = int(
-                np.abs(np.random.normal(loc=150.0, scale=100, size=1)))
-            # распределение размера задачи?
+            class_task_j = classes_tasks[j]
+            if class_task_j == 0:
+                # вычислительная сложность
+                beta = 0.5
+                max_calc_size = 1200000
+
+                # отправка
+                max_send_size = 200
+                sigma_send_size = 10
+
+                # получение
+                max_get_size = 200
+                sigma_get_size = 10
+
+            elif class_task_j == 1:
+                # вычислительная сложность
+                beta = 0.5
+                max_calc_size = 600000
+
+                # отправка
+                max_send_size = 100
+                sigma_send_size = 7
+
+                # получение
+                max_get_size = 100
+                sigma_get_size = 7
+
+            elif class_task_j == 2:
+                # вычислительная сложность
+                beta = 0.5
+                max_calc_size = 240000
+
+                # отправка
+                max_send_size = 30
+                sigma_send_size = 7
+
+                # получение
+                max_get_size = 30
+                sigma_get_size = 7
+
+            else:
+                # вычислительная сложность
+                beta = 0.5
+                max_calc_size = 5000
+
+                # отправка
+                max_send_size = 10.0
+                sigma_send_size = 5
+
+                # получение
+                max_get_size = 10.0
+                sigma_get_size = 5
+
+            # распределение вычислительной сложности задачи
+            calc_size = int((np.random.exponential(beta, size=1) % 1.0) * max_calc_size)
+
+            # распределение размера задачи
             transfer_weight = int(
-                np.abs(np.random.normal(loc=150.0, scale=100, size=1)))
+                np.abs(np.random.normal(loc=max_send_size, scale=sigma_send_size, size=1)))
+            
+            # распределение размера ответа
             transfer_weight_return = int(np.abs(np.random.normal(
-                loc=150.0, scale=100, size=1)))  # распределение размера ответа
+                loc=max_get_size, scale=sigma_get_size, size=1)))
+
             # задаём время, когда должна появится задача по экспоненциальному распределению
             time_to_create = prev_time + \
-                float(np.random.exponential(scale=100.0, size=1))
+                float(np.random.exponential(scale=1000.0, size=1))
             task_j = Task(calc_size, transfer_weight,
                           transfer_weight_return, int(i))
             output.append((i, time_to_create, task_j))
@@ -75,17 +131,17 @@ if __name__ == "__main__":
     # задаём мощность классов узлов
     maxpower = 1
     powers = {
-        1: maxpower,
-        2: maxpower * 0.65,
-        3: maxpower * 0.35,
-        4: maxpower * 0.25,
-        5: maxpower * 0.2,
-        6: maxpower * 0.125,
-        7: maxpower * 0.075,
-        8: maxpower * 0.01
+        1: maxpower, # ноутбук мощный
+        2: maxpower * 0.65, # ноутбук средний
+        3: maxpower * 0.35, # смартфон мощный
+        4: maxpower * 0.25, # планшет средний
+        5: maxpower * 0.2, # смартфон средний
+        6: maxpower * 0.125, # смартфон бюджетный
+        7: maxpower * 0.075, # смарт-телевизор
+        8: maxpower * 0.01 # смарт-часы
     }
     # загружаем json с описанием сценария
-    number_scenario = '2'
+    number_scenario = '4'
     np.random.seed(0)
     file = open(fr'.\scenario\config_scenario_{number_scenario}.json')
     scenario = json.load(file)
@@ -106,74 +162,43 @@ if __name__ == "__main__":
 
         # задаём уравнение движения для узла
         if way_eq == "static":
-            # def way_equation(x0, y0, d, t): return (x0, y0, d)
-            # way_equation = lambda x0, y0, d, t: (x0, y0, d)
-            # way_equation = lambda x0, y0, d, t: (x0, y0, d)
             wq = get_static(x0, y0)
+
         elif way_eq == "circle":
             w = scenario['nodes'][node_id]['w']
-            # её надо хранить в ноде
             direction = scenario['nodes'][node_id]['direction']
             xc, yc = scenario['nodes'][node_id]['xc'], scenario['nodes'][node_id]['yc']
-
-            # def way_equation(x0, y0, d, t): return eq_circle(
-            #     x0, y0, xc, yc, w, d, t)
-
             wq = get_circle(xc, yc, w, direction)
-            # way_equation = lambda x0, y0, d, t: eq_circle(
-            #     x0, y0, xc, yc, w, d, t)
         elif way_eq == "partline":
             x_s, y_s = scenario['nodes'][node_id]['x_start'], scenario['nodes'][node_id]['y_start']
             x_e, y_e = scenario['nodes'][node_id]['x_end'], scenario['nodes'][node_id]['y_end']
             w, direction = scenario['nodes'][node_id]['w'], scenario['nodes'][node_id]['direction']
-
-            # def way_equation(x0, y0, d, t): return eq_partline(
-            #     x0, y0, x_s, y_s, x_e, y_e, w, t, d)
-
             wq = get_partline(x_s, y_s, x_e, y_e, w)
-            # way_equation = lambda x0, y0, d, t: eq_partline(
-            #     x0, y0, copy.deepcopy(x_s), copy.deepcopy(y_s), copy.deepcopy(x_e), copy.deepcopy(y_e), copy.deepcopy(w), t, d)
 
         elif way_eq == "sin_or_cos":
             x_s, x_e = scenario['nodes'][node_id]['x_start'], scenario['nodes'][node_id]['x_end']
             w = scenario['nodes'][node_id]['w']
             its_sin = scenario['nodes'][node_id]['sin']
             y = y0
-
-            # def way_equation(x0, y0, d, t): return eq_sin_or_cos(
-            #     x0, y0, x_s, x_e, y, w, t, d, sin=its_sin)
-
             wq = get_sincos(x_s, x_e, y, w, its_sin)
-            # way_equation = lambda x0, y0, d, t: eq_sin_or_cos(
-            #     x0, y0, x_s, x_e, y, w, t, d, sin=its_sin)
+
         elif way_eq == "brownian":
             n = 1
             w = scenario['nodes'][node_id]['w']
             x_s, y_s = scenario['nodes'][node_id]['x_start'], scenario['nodes'][node_id]['y_start']
             x_e, y_e = scenario['nodes'][node_id]['x_end'], scenario['nodes'][node_id]['y_end']
-
-            # def way_equation(x0, y0, d, t): return constraints_to_brownian(
-            #     brownian(x0, y0, n, t, w), x_s, y_s, x_e, y_e)
-
             wq = get_brown(x_s, y_s, x_e, y_e, w)
-            # way_equation = lambda x0, y0, d, t: constraints_to_brownian(
-            #     brownian(x0, y0, n, t, w), x_s, y_s, x_e, y_e)
-            # way_equations[int(node_id)] = lambda x0, y0, d, t: constraints_to_brownian(
-            #     brownian(x0, y0, n, t, w), scenario['nodes'][node_id]['x_start'], scenario['nodes'][node_id]['y_start'], scenario['nodes'][node_id]['x_end'], scenario['nodes'][node_id]['y_end'])
+
         else:
             print(
                 f"Для узла {node_id} задано не реализованное уравнение движения - {way_eq}.")
             raise ValueError
-        # # задаём узел
-        # nodes[int(node_id)] = Node(scenario['nodes'][node_id]['x'], scenario['nodes']
-        #                            [node_id]['y'], scenario['nodes'][node_id]['power'], way_equation)
+
         nodes[int(node_id)] = Node(x0, y0, power, wq)
 
-    print(nodes)
     # генерируем сценарии
     node_ids = [int(a) for a in list(scenario['nodes'].keys())]
     tasks = generate_tasks(node_ids)
-    # print(tasks)
 
     def bandwidth_formula(max_dist, max_bandwidth): return (
         lambda d: max_bandwidth - d*(max_bandwidth/max_dist))
@@ -189,7 +214,7 @@ if __name__ == "__main__":
     # net.update(104,0.1)
     # print(net.nodes)
     sim = Simulation(tasks=tasks, net=net, step=10,logger=logger)
-    sim.run(20000,(-10, -10, 10, 10),save_dir="test_1") # 200
+    sim.run(5000,(-10, -10, 10, 10),save_dir="test_1") # 200
 
     # 1. Сценарии. Разобрать с генератором задач
     # 2. Переменная хранения состояния сети, интерфейс для использования в Simulator (описан в init класса Net)
