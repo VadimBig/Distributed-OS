526a66915741d779724df0a93a647091c72e125a
vadim <79945323+VadimBig@users.noreply.github.com>
2024-03-22 20:08:43 +0300

    Update brownian.py

--- brownian.py
+++ brownian.py
@@ -4,47 +4,7 @@ import numpy as np
 
 
 def brownian(x0, n: int, dt: float, delta: float, out=None):
-    """
-    Уравнение броуновского движения:
-        X(t) = X(0) + N(0, delta**2 * t; 0, t)
-
-    где N(a,b; t0, t1) нормальная распределённая случайная величина со средним a и
-    дисперсией b. Параметры t0 и t1 make explicit the statistical
-    independence of N on different time intervals; that is, if [t0, t1) and
-    [t2, t3) are disjoint intervals, then N(a, b; t0, t1) and N(a, b; t2, t3)
-    are independent.
-    
-    Written as an iteration scheme,
-
-        X(t + dt) = X(t) + N(0, delta**2 * dt; t, t+dt)
-
-
-    If `x0` is an array (or array-like), each value in `x0` is treated as
-    an initial condition, and the value returned is a numpy array with one
-    more dimension than `x0`.
-
-    Arguments
-    ---------
-    * x0 : float or numpy array (or something that can be converted to a numpy array
-         using numpy.asarray(x0)).
-        The initial condition(s) (i.e. position(s)) of the Brownian motion.
-    * n - число шагов, которые нужно совершить
-    * dt - размер шага во времени
-    * delta -  определяет скорость броуновского движения
-        delta determines the "speed" of the Brownian motion.  The random variable
-        of the position at time t, X(t), has a normal distribution whose mean is
-        the position at time t=0 and whose variance is delta**2*t.
-    * out : numpy array or None
-        If `out` is not None, it specifies the array in which to put the
-        result.  If `out` is None, a new numpy array is created and returned.
-
-    Returns
-    -------
-    A numpy array of floats with shape `x0.shape + (n,)`.
-    
-    Note that the initial value `x0` is not included in the returned array.
-    """
-
+ 
     x0 = np.asarray(x0)
 
     # For each element of x0, generate a sample of n numbers from a
